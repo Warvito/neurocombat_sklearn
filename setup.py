268c9b4e37cafdfdbe3c3822ac9d4c3655474273
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2019-09-08 23:40:28 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='neurocombat-sklearn',
+    version='0.1',
+    scripts=['combat'],
+    author='Walter Hugo Lopez Pinaya',
+    author_email='ianonimato@hotmail.com',
+    description='Harmonizing neuroimaging data across sites. Implementation of neurocombat using sklearn format',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Warvito/combat-sklearn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
