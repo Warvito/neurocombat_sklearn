6e661c22d43d482d9d9be3e18bf96a796ab3b9f4
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2019-10-16 23:09:30 +0100

    Draft for tests

--- neurocombat_sklearn/tests/test_common.py
+++ neurocombat_sklearn/tests/test_common.py
@@ -2,10 +2,10 @@ import pytest
 
 from sklearn.utils.estimator_checks import check_estimator
 
-from sklearn_rvm import RVR, RVC
+from neurocombat_sklearn import CombatModel
 
 @pytest.mark.parametrize(
-    "Estimator", [RVR, RVC]
+    "Estimator", [CombatModel]
 )
-def test_all_estimators(Estimator):
+def test_all_transformers(Estimator):
     return check_estimator(Estimator)
